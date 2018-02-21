ec1b2b8ee69da2ee50f24e196efa3eec20493ebb
DESKTOP-6GQ38RC\Tugay <tgy.uner@gmail.com>
2018-02-21 15:41:48 +0300

    Initial commit

---
+++ SingletonExample/Ogrenci.cs
@@ -0,0 +1,32 @@
+﻿namespace SingletonExample
+{
+   public class Ogrenci
+    {
+        // Tugay ÜNER 2018 ©
+
+        public string OgrenciAd { get; set; }
+        public string OgrenciSoyad { get; set; }
+
+        private static Ogrenci _ogrenci;
+
+        private Ogrenci()
+        {
+
+        }
+
+        public static Ogrenci Instance()
+        {
+            if (_ogrenci==null)
+            {
+                _ogrenci = new Ogrenci();
+                return _ogrenci;
+            }
+            else
+            {
+                return _ogrenci;
+            }
+        }
+
+
+    }
+}
